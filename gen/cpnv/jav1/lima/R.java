2aa05a2695762b76278069c5bbd5f55ff8ba979a
Steven Catteaux <stevenrvn@gmail.com>
2014-05-19 12:07:05 +0200

    Added View Item

--- gen/cpnv/jav1/lima/R.java
+++ gen/cpnv/jav1/lima/R.java
@@ -28,19 +28,23 @@ public final class R {
         public static final int cmdMainDistribution=0x7f060009;
         public static final int cmdMainInventory=0x7f060008;
         public static final int cmdMainValidation=0x7f060007;
+        public static final int cmdMainViewItem=0x7f06000b;
         public static final int goAct1=0x7f060000;
         public static final int goAct2=0x7f060001;
         public static final int outputzone=0x7f060003;
         public static final int scrollViewDebug=0x7f060002;
+        public static final int scrollViewItemView=0x7f06000c;
     }
     public static final class layout {
         public static final int debug=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int viewitem=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050005;
         public static final int debTitle=0x7f050000;
         public static final int empty=0x7f050009;
+        public static final int itemTitle=0x7f05000d;
         public static final int uitDebugAction1=0x7f050002;
         public static final int uitDebugAction2=0x7f050003;
         public static final int uitDebugAction3=0x7f050004;
@@ -49,6 +53,11 @@ public final class R {
         public static final int uitMainDebug=0x7f050001;
         public static final int uitMainDistribution=0x7f050008;
         public static final int uitMainInventory=0x7f050007;
+        public static final int uitMainItemView=0x7f05000e;
         public static final int uitMainValidation=0x7f050006;
+        public static final int uitMainViewItem=0x7f05000c;
+        public static final int uitViewItemAction1=0x7f05000f;
+        public static final int uitViewItemAction2=0x7f050010;
+        public static final int uitViewItemAction3=0x7f050011;
     }
 }
