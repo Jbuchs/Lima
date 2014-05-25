d089a22feb550e21c256fb15e3d83c529c6eea95
MisterX25 <xavier.carrel@cpnv.ch>
2014-05-25 14:18:02 +0200

    ok

--- src/cpnv/jav1/lima/Gear.java
+++ src/cpnv/jav1/lima/Gear.java
@@ -33,6 +33,19 @@ public class Gear extends Article{
 		return super.dump()+", size : "+_size;
 	}
 	
+	/**
+	 * Compare this gear object to another.
+	 * The comparison order is: 
+	 *   - Alphabetically on the name
+	 *   - On the size (XS,S,M,L,XL,XXL)
+	 *   - Alphabetically on the supplier name
+	 * @param other
+	 * The other Gear to which we compare this object
+	 * @return
+	 * 1 if 'this' is greater than 'other'
+	 * 0 if 'this' is equal to 'other'
+	 * -1 if 'this' is smaller than 'other'
+	 */
 	public int compareTo (Gear other) {
 		return 0;
 	}
